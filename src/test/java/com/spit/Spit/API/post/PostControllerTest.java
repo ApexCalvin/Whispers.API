15b0a0b0e366d338a5dcea349623a3b0fe6b2cd7
ApexCalvin-WinOS <ApexCalvin@gmail.com>
2023-11-02 20:13:08 -0400

    attempted

--- src/test/java/com/spit/Spit/API/post/PostControllerTest.java
+++ src/test/java/com/spit/Spit/API/post/PostControllerTest.java
@@ -2,8 +2,11 @@ package com.spit.Spit.API.post;
 
 import com.spit.Spit.API.Account.Account;
 import com.spit.Spit.API.Account.AccountRepository;
+import com.spit.Spit.API.Account.AccountServices;
 import com.spit.Spit.API.Post.CreatePostDTO;
+import com.spit.Spit.API.Post.Post;
 import com.spit.Spit.API.Post.PostController;
+import com.spit.Spit.API.Post.PostServices;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -13,6 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Date;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
@@ -24,24 +28,31 @@ public class PostControllerTest {
     PostController postController;
 
     @Mock
-    AccountRepository accountRepository;
+    PostServices postServices;
 
+    @Mock
+    AccountServices accountServices;
 
     @Test
     void createPost(){
-        //Before
-        Account account = new Account("John", "Doe");
-        account.setAccount_id(1L);
-        when(accountRepository.save(any(Account.class))).thenReturn(account);
 
+        //created DTO --> Post Request
         CreatePostDTO postDTO = new CreatePostDTO();
         postDTO.setAccountId(1L);
         postDTO.setMessage("Hello");
+
+        //HardCode: Assume return object
+        Account account = new Account("John", "Doe");
+        account.setAccount_id(1L);
+        Optional<Account> optionalAcc = Optional.of(account);
+        when(accountServices.getAccountById(postDTO.getAccountId())).thenReturn(optionalAcc);
 
 
         ResponseEntity<String> re = postController.createPost(postDTO);
 
         assertThat(re.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+        verify(postServices).createPost(any(Post.class));
     }
 
     void createPost_AccountNotFound(){}
