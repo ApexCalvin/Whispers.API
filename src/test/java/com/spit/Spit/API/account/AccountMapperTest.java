5341547cc6c2875cd93f724cec8a26dd30dbae8e
Eryk Liszewski <liszewski31098@gmail.com>
2023-11-07 20:27:49 -0500

    WIP add fromCreatePostDTO

--- src/test/java/com/spit/Spit/API/account/AccountMapperTest.java
+++ src/test/java/com/spit/Spit/API/account/AccountMapperTest.java
@@ -1,13 +1,25 @@
 package com.spit.Spit.API.account;
 
 import com.spit.Spit.API.Account.Account;
-import com.spit.Spit.API.Tools.DtoMapper;
+import com.spit.Spit.API.Account.AccountServices;
 import com.spit.Spit.API.Account.CreateAccountDTO;
+import com.spit.Spit.API.Post.CreatePostDTO;
+import com.spit.Spit.API.Post.Post;
+import com.spit.Spit.API.Tools.DtoMapper;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class AccountMapperTest {
+    @Mock
+    private AccountServices accountServices;
+
     @Test
     void fromCreateAccountDTO_standardTranslation(){
         CreateAccountDTO expected = new CreateAccountDTO();
@@ -18,5 +30,21 @@ public class AccountMapperTest {
 
         assertThat(actual.getName()).isEqualTo(expected.getName());
         assertThat(actual.getHandle()).isEqualTo(expected.getHandle());
+    }
+
+    @Disabled
+    @Test
+    void fromCreatePostDTO_standardTranslation(){
+        CreatePostDTO expected = new CreatePostDTO();
+        expected.setAccountId(1L);
+        expected.setMessage("FakeMessage");
+//        AccountServices accountServices = mock(AccountServices.class);
+        Account account = new Account();
+
+        when(accountServices.getExistingAccount(expected.getAccountId())).thenReturn(account);
+        Post actual = DtoMapper.fromCreatePostDTO(expected);
+
+        assertThat(actual.getAccount()).isEqualTo(account);
+        assertThat(actual.getMessage()).isEqualTo(expected.getMessage());
     }
 }
