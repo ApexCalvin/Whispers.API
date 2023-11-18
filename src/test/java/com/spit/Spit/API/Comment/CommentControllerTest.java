5864782b9af43206569ccf198353269e49ec8123
Eryk Liszewski <liszewski31098@gmail.com>
2023-11-18 10:16:45 -0500

    setup comment test classes

--- src/test/java/com/spit/Spit/API/Comment/CommentControllerTest.java
+++ src/test/java/com/spit/Spit/API/Comment/CommentControllerTest.java
@@ -1,4 +1,17 @@
 package com.spit.Spit.API.Comment;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class CommentControllerTest {
+
+    @InjectMocks
+    private CommentController subject;
+
+    @Mock
+    private CommentService commentService;
+
 }
--- src/test/java/com/spit/Spit/API/Comment/CommentServiceTest.java
+++ src/test/java/com/spit/Spit/API/Comment/CommentServiceTest.java
@@ -1,4 +1,15 @@
 package com.spit.Spit.API.Comment;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class CommentServiceTest {
+    @InjectMocks
+    private CommentService subject;
+
+    @Mock
+    private CommentRepository commentRepository;
 }
