541069c6973ac6a72f15a0908638f26647b379f1
Eryk Liszewski <liszewski31098@gmail.com>
2023-12-01 17:56:39 -0500

    finish LikeControllerTest

--- src/test/java/com/spit/Spit/API/like/LikeControllerTest.java
+++ src/test/java/com/spit/Spit/API/like/LikeControllerTest.java
@@ -8,6 +8,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
@@ -46,11 +49,13 @@ public class LikeControllerTest {
     @Test
     void getLikeById_likeExists_OkResponse(){
         Long id = 1L;
-        when(likeService.getLikeById(id)).thenReturn(new Like_());
+        Like_ expected = new Like_();
+        when(likeService.getLikeById(id)).thenReturn(expected);
 
         ResponseEntity<Like_> actual = subject.getLikeById(id);
 
         assertThat(actual.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(actual.getBody()).isEqualTo(expected);
     }
 
     @Test
@@ -63,4 +68,59 @@ public class LikeControllerTest {
         assertThat(actual.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 
+    @Test
+    void getLikeByHandleAndPost_likeDoesNotExists_NotFoundResponse(){
+        Long id = 1L;
+        String handle = "MarvelLover";
+        when(likeService.getLikeByHandleAndPost(handle, id)).thenReturn(null);
+
+        ResponseEntity<Like_> actual = subject.getLikeByHandleAndPost(handle, id);
+
+        assertThat(actual.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    void getLikeByHandleAndPost_likeExists_OkResponse(){
+        Long id = 1L;
+        String handle = "Butcher";
+        Like_ expected = new Like_();
+        when(likeService.getLikeByHandleAndPost(handle, id)).thenReturn(expected);
+
+        ResponseEntity<Like_> actual = subject.getLikeByHandleAndPost(handle, id);
+
+        assertThat(actual.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(actual.getBody()).isEqualTo(expected);
+    }
+
+    @Test
+    void getAllLikes_OkResponse(){
+        List<Like_> expected = List.of(new Like_());
+        when(likeService.getAllLikes()).thenReturn(expected);
+
+        ResponseEntity<List<Like_>> actual = subject.getAllLikes();
+
+        verify(likeService).getAllLikes();
+        assertThat(actual.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(actual.getBody()).isEqualTo(expected);
+    }
+
+    @Test
+    void deleteLike_likeDeleted_NoContentResponse(){
+        Long id = 1L;
+        when(likeService.deleteLike(id)).thenReturn(true);
+
+        ResponseEntity<Void> actual = subject.deleteLike(id);
+
+        assertThat(actual.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+    }
+
+    @Test
+    void deleteLike_likeNotDeleted_NotFoundResponse(){
+        Long id = 1L;
+        when(likeService.deleteLike(id)).thenReturn(false);
+
+        ResponseEntity<Void> actual = subject.deleteLike(id);
+
+        assertThat(actual.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
 }
