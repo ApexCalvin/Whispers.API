77466f8689de4b0e66319e7870cf9aa268ec1d69
Calvin -Win10 <ApexCalvin@gmail.com>
2023-12-29 19:44:56 -0500

    worked on searchHandle() in UI

--- src/main/resources/static/services/navbar.js
+++ src/main/resources/static/services/navbar.js
@@ -3,18 +3,43 @@ function searchMenu() {
 
     const searchMenu =  `
                         <div id="search-Menu" class="postBlockFromFeed">
-                            <input>
+                            <input id="search-value">
                             <button onClick="searchHandle()">Handle Search</button>
                             <button onClick="searchHashtag()";>Hashtag Search</button>
                         </div>
                         `;
 
-    const display = document.getElementById('nav-select');
+    const display = document.getElementById('search-div');
     display.innerHTML = searchMenu;
 }
 
 function searchHandle() {
-    console.log("searchHandle method")
+    console.log("searchHandle method");
+
+    //let searchValue = document.getElementById("search-value");
+    let searchValue = "first";
+
+    let url = "/hashtag/"+searchValue+"/posts";
+
+    fetch(url)
+        .then(response => response.json()) // Parse the response as JSON
+        .then(data => {
+            const feed = data.map(object => {
+                return `
+                    <div class="postBlockFromFeed" onClick="loadSelectedPost(${object.id})">
+                        <h4 style="text-align: center; color: #1190E7;">${object.name} @${object.handle}</h2>
+                        <h4 style="text-align: center;">${object.message}</h4>
+                        <p style="text-align: center;">${moment(object.date).format('MMMM Do YYYY, h:mm:ss a')}</p>
+                    </div>
+                `;
+            });
+
+            const display = document.getElementById('column-details');
+            display.innerHTML = feed.join('');
+        })
+        .catch(error => {
+            console.error('Error fetching data: ', error)
+        });
 }
 
 function searchHashtag() {
@@ -33,7 +58,7 @@ function accountsMenu() {
             const accounts = data.map(object => {
                 return `
                     <div class="postBlockFromFeed" onClick="setActiveUser(${object.id})">
-                        <img id="accounts-photo" src="./images/${object.id}.jpg" alt="Image of Profile" style="border-radius: 50%; width: 10px; height: auto;">
+                        <img id="accounts-photo" src="./images/${object.id}.jpg" alt="Image of Profile" style="border-radius: 50%; width: 100px; height: auto;">
                         <div>
                             <h4 style="text-align: center; color: #1190E7;">${object.name}</h2>
                             <p style="text-align: center; color: #1190E7;">@${object.handle}<p>
