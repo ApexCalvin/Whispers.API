21a3b7ff574f41754a123d3e939c91a59060179f
Calvin -Win10 <ApexCalvin@gmail.com>
2023-12-30 17:36:19 -0500

    worked on searchHashtag() in UI

--- src/main/resources/static/services/navbar.js
+++ src/main/resources/static/services/navbar.js
@@ -17,9 +17,9 @@ function searchHandle() {
     console.log("searchHandle method");
 
     //let searchValue = document.getElementById("search-value");
-    let searchValue = "first";
-
-    let url = "/hashtag/"+searchValue+"/posts";
+    let searchValue = "homelander";
+ 
+    let url = "/handle/" + searchValue;
 
     fetch(url)
         .then(response => response.json()) // Parse the response as JSON
@@ -44,6 +44,31 @@ function searchHandle() {
 
 function searchHashtag() {
     console.log("searchHashtag method")
+
+    //let searchValue = document.getElementById("search-value");
+    let searchValue = "first";
+
+    let url = "/hashtag/"+searchValue+"/posts";
+
+    fetch(url)
+    .then(response => response.json()) // Parse the response as JSON
+    .then(data => {
+        const feed = data.map(object => {
+            return `
+                <div class="postBlockFromFeed" onClick="loadSelectedPost(${object.id})">
+                    <h4 style="text-align: center; color: #1190E7;">${object.name} @${object.handle}</h2>
+                    <h4 style="text-align: center;">${object.message}</h4>
+                    <p style="text-align: center;">${moment(object.date).format('MMMM Do YYYY, h:mm:ss a')}</p>
+                </div>
+            `;
+        });
+
+        const display = document.getElementById('column-details');
+        display.innerHTML = feed.join('');
+    })
+    .catch(error => {
+        console.error('Error fetching data: ', error)
+    });
 }
 
 function accountsMenu() {
