61e4293cae1be125fc5fa3f9ce7d897359a3a1be
Calvin-Mac <ApexCalvin@gmail.com>
2023-12-31 16:44:21 -0500

    wip, createPost() in UI

--- src/main/resources/static/services/column-profile.js
+++ src/main/resources/static/services/column-profile.js
@@ -15,7 +15,7 @@ function loadProfile() {
                                         <input id="new-comment-message" placeholder="Type Message Here">
                                         <br>
                                         <br>
-                                        <button onClick="console.log('Button Clicked!');">Submit</button>
+                                        <button onClick="createPost()">Submit</button>
                                     </form>
                                 </div>
                                 `                
@@ -28,5 +28,48 @@ function loadProfile() {
         })
         .catch(error => {
             console.error('Error fetching data: ', error)
+        });
+}
+
+function createPost() {
+    console.log('createPost method');
+
+    const msg = document.getElementById("new-comment-message").value;
+
+    // Data to be sent in the request body
+    const postData = {
+        accountid: activeUserId,
+        message: msg,
+        hashtags: []
+    };
+
+    // Define the fetch options for the POST request
+    const fetchOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            // Add any other headers as needed
+        },
+        body: JSON.stringify(postData), // Convert the data to a JSON string
+    };
+
+    // Make the POST request using the Fetch API
+    fetch("/post", fetchOptions)
+        .then(response => {
+            // Check if the request was successful (status code 200-299)
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            // Parse the response JSON
+            return response.json();
+        })
+        .then(data => {
+            // Handle the data from the response
+            console.log('POST request successful:', data);
+        })
+        .catch(error => {
+            // Handle any errors that occurred during the fetch
+            console.error('POST request error:', error);
         });
 }
